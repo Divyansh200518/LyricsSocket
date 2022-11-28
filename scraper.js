8a233272268d6d9c452ca53d249edbe03a1f4dda
Divyansh200518 <87868119+Divyansh200518@users.noreply.github.com>
2022-11-28 19:06:19 +0530

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -67,7 +67,7 @@ async function scrapeSong(url) {
 
 
 const http = require('http').createServer();
-const port = 3000;
+const port = process.env.PORT;
 const hostname = '0.0.0.0';
 const io = require('socket.io')(http,{
     cors:{origin: "*"}
